d109cbd0995fb2f88a83ae1fc20cb3df3bf0b2d0
Ivan Sokcevic <gmail@isokcevic.com>
2021-12-07 14:14:04 +0100

    Further clean up 01

--- 01/main.rb
+++ 01/main.rb
@@ -1,7 +1,7 @@
 lines = File.open("input"){|f| f.readlines(chomp: true)}.map(&:to_i)
 
 def num_increases(numbers)
-  numbers.each_cons(2).reduce(0) {|increases, nums| nums[1] > nums[0] ? increases + 1 : increases}
+  numbers.each_cons(2).select{|nums| nums[1] > nums[0]}.count
 end
 pp num_increases(lines)
 
