01784f8deb314cb874e9cff7bbe48d86ab51f4e8
Ivan Sokcevic <gmail@isokcevic.com>
2021-12-11 08:19:22 +0100

    DAY-11 - simplify implementation

--- 11/main.rb
+++ 11/main.rb
@@ -1,50 +1,50 @@
-def flash(oct_map, flash_map, x, y)
-  return 0 if oct_map[y][x] < 10 || flash_map[y][x]
-  flash_map[y][x] = true
-  flash_count = 1
-  (-1..1).each do |dy|
-    next if y + dy < 0 || y + dy >= oct_map.length
-    (-1..1).each do |dx|
-      next if dx == 0 && dy == 0 || x + dx < 0 || x + dx >= oct_map[0].length
-      oct_map[y + dy][x + dx] += 1
-      flash_count += flash(oct_map, flash_map, x+dx, y+dy)
+def increase(oct_map, x, y)
+  flash_count = 0
+  oct_map[y][x] += 1
+
+  if oct_map[y][x] == 10
+    flash_count = 1
+    (-1..1).each do |dy|
+      next if y + dy < 0 || y + dy >= oct_map.length
+      (-1..1).each do |dx|
+        next if dx == 0 && dy == 0 || x + dx < 0 || x + dx >= oct_map[0].length
+        flash_count += increase(oct_map, x+dx, y+dy)
+      end
     end
   end
 
   flash_count
 end
 
-def reset(oct_map, flash_map)
+def reset(oct_map)
   oct_map.each.with_index do |r, i|
     r.each.with_index do |_, j|
       oct_map[i][j] = 0 if oct_map[i][j] > 9
-      flash_map[i][j] = false
     end
   end
 end
 
 oct = File.open("input", "r"){|f| f.readlines(chomp:true).map{|l| l.split("").map(&:to_i)}}
 
-flash_map = oct.map{|r| [false] * r.length}
-
-flash_count = 0
+total_flashes = 0
 step = 0
 
 loop do
   step += 1
+  step_flashes = 0
 
   oct.each.with_index do |r, i|
     r.each.with_index do |_, j|
-      oct[i][j] += 1
-      flash_count += flash(oct, flash_map, j, i)
+      step_flashes += increase(oct, j, i)
     end
   end
 
-  pp flash_count if step == 100
+  total_flashes += step_flashes
+  pp total_flashes if step == 100
 
-  break if flash_map.all?{|r| r.all?}
+  break if step_flashes == oct.length * oct[0].length
 
-  reset(oct, flash_map)
+  reset(oct) if step_flashes > 0
 end
 
 pp step
