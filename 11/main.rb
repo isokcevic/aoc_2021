ce8a23d1e7319c3053cfabc203c366fe2002ae43
Ivan Sokcevic <gmail@isokcevic.com>
2021-12-11 08:54:14 +0100

    DAY-11 - work with coords instead of score - allows cleaner reset

--- 11/main.rb
+++ 11/main.rb
@@ -1,26 +1,24 @@
 def increase(oct_map, x, y)
-  flash_count = 0
   oct_map[y][x] += 1
-
   if oct_map[y][x] == 10
-    flash_count = 1
+    flash_coords = [[x, y]]
     (-1..1).each do |dy|
       next if y + dy < 0 || y + dy >= oct_map.length
       (-1..1).each do |dx|
         next if dx == 0 && dy == 0 || x + dx < 0 || x + dx >= oct_map[0].length
-        flash_count += increase(oct_map, x+dx, y+dy)
+        flash_coords += increase(oct_map, x+dx, y+dy)
       end
     end
-  end
 
-  flash_count
+    flash_coords
+  else
+    []
+  end
 end
 
-def reset(oct_map)
-  oct_map.each.with_index do |r, i|
-    r.each.with_index do |_, j|
-      oct_map[i][j] = 0 if oct_map[i][j] > 9
-    end
+def reset(oct_map, flash_coords)
+  flash_coords.each do |(x, y)|
+    oct_map[y][x] = 0
   end
 end
 
@@ -31,20 +29,20 @@ step = 0
 
 loop do
   step += 1
-  step_flashes = 0
+  flash_coords = []
 
   oct.each.with_index do |r, i|
     r.each.with_index do |_, j|
-      step_flashes += increase(oct, j, i)
+      flash_coords += increase(oct, j, i)
     end
   end
 
-  total_flashes += step_flashes
+  total_flashes += flash_coords.count
   pp total_flashes if step == 100
 
-  break if step_flashes == oct.length * oct[0].length
+  break if flash_coords.count == oct.length * oct[0].length
 
-  reset(oct) if step_flashes > 0
+  reset(oct, flash_coords)
 end
 
 pp step
