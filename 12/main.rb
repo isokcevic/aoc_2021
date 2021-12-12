3d2f4464f47a88bafb31d2f276cb06c1b90bdcfb
Ivan Sokcevic <gmail@isokcevic.com>
2021-12-12 20:36:23 +0100

    DAY-12 - add a nice monkeypatch for even more rubyness

--- 12/main.rb
+++ 12/main.rb
@@ -1,3 +1,9 @@
+class String
+  def uppercase?
+    self == upcase
+  end
+end
+
 class CaveSystem < Hash
   def initialize(tunnels)
     super() { |h, k| h[k] = { :connections => [] } }
@@ -20,11 +26,11 @@ end
 
 cs = CaveSystem.new(File.open("input", "r") { |f| f.readlines(chomp: true) })
 
-pp cs.paths_count { |c, visited| c == c.upcase || !visited.include?(c) }
+pp cs.paths_count { |c, visited| c.uppercase? || !visited.include?(c) }
 
 # Needs an intermediate variable, otherwise do..end block gets passed to pp rather than our function. TIL.
 # Alternative format would be pp (cs.paths_count do ... end ) - but that is bloody ugly. Worse than a too long one-liner.
 r2 = cs.paths_count do |c, visited|
-  c == c.upcase || c != 'start' && (!visited.include?(c) || visited.tally.all? { |k, v| k == k.upcase || v == 1 })
+  c == c.upcase || c != 'start' && (!visited.include?(c) || visited.tally.all? { |k, v| k.uppercase? || v == 1 })
 end
 pp r2
